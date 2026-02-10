50eb0c4a5dfcd3479dc415004d67703bc121ffbf
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-02-10 20:00:52 +0700

    Сделать задание

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,74 @@
+package ru.yandex.practicum.filmorate.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.annotation.TestOnly;
+import ru.yandex.practicum.filmorate.exceptions.ConditionsNotMetException;
+import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.model.Validator;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+    private final Map<Long, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        Validator.validateUser(user);
+        if (isExist(user)) {
+            throw new ValidationException("Пользователь с таким email уже существует");
+        }
+        user.setId(getNextId());
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User update(@RequestBody User newUser) {
+        if (newUser.getId() == null) {
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+
+        if (users.containsKey(newUser.getId())) {
+            User oldUser = users.get(newUser.getId());
+            BeanUtils.copyProperties(newUser, oldUser, "id");
+            return oldUser;
+        }
+        throw new NotFoundException("Пользователь с id = " + newUser.getId() + " не найден");
+    }
+
+    // вспомогательный метод для генерации идентификатора
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+    @TestOnly
+    public void clearData () {
+        users.clear();
+    }
+
+    private boolean isExist (User user) {
+        return users.values().stream().anyMatch(u -> u.getEmail().equalsIgnoreCase(user.getEmail()));
+    }
+
+}
