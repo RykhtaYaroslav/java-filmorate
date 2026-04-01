163105af20a4e3931c5991c4c3f5a346e45d3b75
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-04-01 22:40:25 +0700

    refactor: enforce email uniqueness in user schema
    - **Schema Update**: Added `UNIQUE` constraint to the `email` column in the `users` table.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS film_genres (
 
 CREATE TABLE IF NOT EXISTS users (
     id       BIGINT AUTO_INCREMENT PRIMARY KEY,
-    email    VARCHAR(255) NOT NULL,
+    email    VARCHAR(255) NOT NULL UNIQUE,
     login    VARCHAR(255) NOT NULL,
     name     VARCHAR(255),
     birthday DATE
