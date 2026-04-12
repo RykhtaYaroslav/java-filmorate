fcbdd60fc49bf59b7290a0dde9829dbfe21e6656
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-04-12 18:09:38 +0700

    ``` refactor(genre): remove redundant GenreDbStorage
    Deleted `GenreDbStorage` class, which was previously responsible for
    direct database access for genre entities.
    
    - `GenreService` has been updated to use `FilmGenreRepository` for
      all genre-related data operations, completing the migration to the
      new repository abstraction.
    - This change finalizes the decoupling of genre management introduced
      in a previous commit, improving separation of concerns.
    ```

--- src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.dal.repositories.genre.GenreDbStorage;
+import ru.yandex.practicum.filmorate.dal.repositories.genre.FilmGenreRepository;
 import ru.yandex.practicum.filmorate.dto.genre.GenreDto;
 import ru.yandex.practicum.filmorate.dto.mappers.GenreMapper;
 import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
@@ -13,16 +13,16 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class GenreService {
-    private final GenreDbStorage storage;
+    private final FilmGenreRepository filmGenreRepository;
 
     public List<GenreDto> findAll() {
-        List<Genre> genreList = storage.findAll();
+        List<Genre> genreList = filmGenreRepository.findAll();
 
         return genreList.stream().map(GenreMapper::mapToDto).toList();
     }
 
     public GenreDto findById(int id) {
-        Genre genre = storage.findById(id)
+        Genre genre = filmGenreRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException(String.format("Жанр с id %d не найден", id)));
 
         return GenreMapper.mapToDto(genre);
