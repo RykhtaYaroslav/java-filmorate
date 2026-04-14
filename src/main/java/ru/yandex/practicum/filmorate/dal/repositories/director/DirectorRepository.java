87dba99476c96c8c7f7269b73a274f9c82f6b45b
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-04-15 02:32:14 +0700

    feat(director): add DirectorRepository interface (add-director)
    Introduces the DirectorRepository interface, defining the contract
    for managing Director entities. This interface specifies methods
    for creating, updating, deleting, and retrieving Director objects.

---
+++ src/main/java/ru/yandex/practicum/filmorate/dal/repositories/director/DirectorRepository.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.filmorate.dal.repositories.director;
+
+import ru.yandex.practicum.filmorate.model.Director;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface DirectorRepository {
+    Director create(Director director);
+
+    Director update(Director director);
+
+    void delete(Long id);
+
+    Collection<Director> findAll();
+
+    Optional<Director> findById(Long id);
+}
