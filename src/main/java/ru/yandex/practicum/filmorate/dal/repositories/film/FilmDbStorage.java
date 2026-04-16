06180d9c6a6a9be5b578cf4093be1b4f26e3443d
andreikuznetsov <laskomagia2@yandex.ru>
2026-04-16 15:28:07 +0300

    Group project task: "add recommendations" solution

--- src/main/java/ru/yandex/practicum/filmorate/dal/repositories/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/repositories/film/FilmDbStorage.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.dal.repositories.film;
 
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
